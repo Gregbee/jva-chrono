2564f44d53b3c3822a94d64f33cb6566f2d4525a
Gregbee <gregbee@outlook.com>
2018-04-18 09:00:59 +0200

    java.util.date impl

--- src/main/java/fr/wildcodeschool/quetes/chrono/JavaDateTimeProvider.java
+++ src/main/java/fr/wildcodeschool/quetes/chrono/JavaDateTimeProvider.java
@@ -1,4 +1,94 @@
 package fr.wildcodeschool.quetes.chrono;
 
-public class JavaDateTimeProvider {
+import java.util.Date;
+
+public class JavaDateTimeProvider implements TimeProvider {
+
+    private Date startDate;
+    private Date stopDate;
+    private boolean started;
+    private long totalSecondRuntime;
+    private int secondStoped;
+    private int minutes;
+    private int seconds;
+
+    public JavaDateTimeProvider(){
+        started = false;
+        totalSecondRuntime = 0;
+        secondStoped = 0;
+        minutes = 0;
+        seconds = 0;
+
+    }
+
+    @Override
+    public void startStop() {
+       if(!isStarted()){
+           setStarted(true);
+           startDate = startDate != null ? startDate : new Date();
+
+       }
+       else{
+           setStarted(false);
+
+       }
+
+    }
+
+    private void setStarted(boolean b) {
+        started = b;
+    }
+
+
+
+    @Override
+    public void reset() {
+        totalSecondRuntime = 0;
+        if(isStarted()){
+            startDate = new Date();
+        }
+        secondStoped = 0;
+
+
+    }
+
+    @Override
+    public boolean isStarted() {
+        return started;
+    }
+
+    @Override
+    public long getSecondsTotalRuntime() {
+        //the timer has never been started or has been reset.
+        if(!isStarted() && startDate == null){
+            return totalSecondRuntime;
+        }
+
+       if(!isStarted()){
+           secondStoped++;
+       }
+       else{
+
+           totalSecondRuntime = ((new Date().getTime() - startDate.getTime()) / 1000) - secondStoped;
+
+       }
+        System.out.println(totalSecondRuntime);
+       return totalSecondRuntime;
+
+    }
+
+    @Override
+    public long getHoursRuntime() {
+        return totalSecondRuntime / (60 * 60);
+    }
+
+    @Override
+    public long getMinutesRuntime() {
+        return totalSecondRuntime / 60;
+    }
+
+    @Override
+    public long getSecondsRuntime() {
+        return totalSecondRuntime % 60;
+    }
 }
--- src/main/java/fr/wildcodeschool/quetes/chrono/Startup.java
+++ src/main/java/fr/wildcodeschool/quetes/chrono/Startup.java
@@ -3,7 +3,7 @@ package fr.wildcodeschool.quetes.chrono;
 public class Startup {
 
     public static void main(String... args) throws InterruptedException {
-        TimeProvider tp = new DummyTimeProvider();
+        TimeProvider tp = new JavaDateTimeProvider();
         new Chrono(tp).roll();
     }
 }

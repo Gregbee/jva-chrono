b81b8c108d1ebbc849ec4d796319f8f3e6590bee
Gregbee <gregbee@outlook.com>
2018-04-23 14:58:19 +0200

    nullpointer exeption ok

--- src/main/java/fr/wildcodeschool/quetes/chrono/JavaTimeTimeProvider.java
+++ src/main/java/fr/wildcodeschool/quetes/chrono/JavaTimeTimeProvider.java
@@ -66,7 +66,7 @@ public class JavaTimeTimeProvider implements TimeProvider {
             secondStoped++;
         }
         else{
-
+            startDate = startDate != null ? startDate : Instant.now();
             totalSecondRuntime = ((Instant.now().toEpochMilli() - startDate.toEpochMilli()) / 1000) - secondStoped + initCounter;
 
         }
